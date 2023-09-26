a6f5f8b3b9311ef60fb4c97b317f85db27a74b4f
haymee <dev.haymee@gmail.com>
2023-09-27 00:31:54 +0900

    UI 레이아웃 작업

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,38 +2,39 @@ import '../globals.css';
 import './style.css';
 import React from 'react';
 import {
+  CaretDoubleLeft,
   CaretDown,
   CaretRight,
   Gift,
   Graph,
   Info,
-  ToggleLeft,
   ShoppingCart,
+  SmileyAngry,
   User,
 } from '@phosphor-icons/react/dist/ssr';
 import Image from 'next/image';
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className={'wrapper-box'}>
       <div className={'header-box'}>
         <div className={'header-left-box'}>
-          <Image
-            src={
-              'https://zzimcar.s3.ap-northeast-2.amazonaws.com/common/image/logo/logo_white.svg'
-            }
-            alt={'zzimcar-logo'}
-            width={88}
-            height={22}
-          ></Image>
-          <ToggleLeft size={20} color={'#CBDAFF'} weight={'fill'}></ToggleLeft>
+          <div className={'header-logo-box'}>
+            <Image
+              src={'https://zzimcar.s3.ap-northeast-2.amazonaws.com/common/image/logo/logo_white.svg'}
+              alt={'zzimcar-logo'}
+              width={88}
+              height={22}
+            ></Image>
+            <SmileyAngry size={24} color={'#FFFFFF'} weight={'regular'}></SmileyAngry>
+          </div>
+          <CaretDoubleLeft size={20} color={'#CBDAFF'} weight={'bold'}></CaretDoubleLeft>
         </div>
         <div className={'header-right-box'}>
-          <div className={'profile-box'}>profile-box</div>
+          <div className={'profile-box'}>
+            <span>박혜미(haymee@naturemobility.com)</span>
+            <CaretDown size={12} color={'#565758'}></CaretDown>
+          </div>
         </div>
       </div>
       <div className={'body-box'}>
@@ -45,34 +46,18 @@ export default function DashboardLayout({
                 <Info size={16} color={'#CBDAFF'} weight={'regular'}></Info>
                 <span>컨텐츠 관리</span>
               </div>
-              <CaretDown
-                size={12}
-                color={'#CBDAFF'}
-                weight={'bold'}
-              ></CaretDown>
+              <CaretDown size={12} color={'#CBDAFF'} weight={'bold'}></CaretDown>
             </div>
             <div className={['nav-sub-box', 'sub-active'].join(' ')}>
-              <CaretRight
-                size={12}
-                color={'#CBDAFF'}
-                weight={'duotone'}
-              ></CaretRight>
+              <CaretRight size={12} color={'#CBDAFF'} weight={'duotone'}></CaretRight>
               <span>공지사항</span>
             </div>
             <div className={'nav-sub-box'}>
-              <CaretRight
-                size={12}
-                color={'#CBDAFF'}
-                weight={'duotone'}
-              ></CaretRight>
+              <CaretRight size={12} color={'#CBDAFF'} weight={'duotone'}></CaretRight>
               <span>배너</span>
             </div>
             <div className={'nav-sub-box'}>
-              <CaretRight
-                size={12}
-                color={'#CBDAFF'}
-                weight={'duotone'}
-              ></CaretRight>
+              <CaretRight size={12} color={'#CBDAFF'} weight={'duotone'}></CaretRight>
               <span>이벤트</span>
             </div>
             <div className={'nav-item-box'}>
@@ -80,11 +65,7 @@ export default function DashboardLayout({
                 <Graph size={16} color={'#CBDAFF'} weight={'regular'}></Graph>
                 <span>예약 통계</span>
               </div>
-              <CaretRight
-                size={12}
-                color={'#CBDAFF'}
-                weight={'bold'}
-              ></CaretRight>
+              <CaretRight size={12} color={'#CBDAFF'} weight={'bold'}></CaretRight>
             </div>
             <div className={'nav-item-box'}>
               <div className={'nav-item-name-box'}>
@@ -103,29 +84,17 @@ export default function DashboardLayout({
             <h5>CS 센터</h5>
             <div className={'nav-item-box'}>
               <div className={'nav-item-name-box'}>
-                <ShoppingCart
-                  size={16}
-                  color={'#CBDAFF'}
-                  weight={'regular'}
-                ></ShoppingCart>
+                <ShoppingCart size={16} color={'#CBDAFF'} weight={'regular'}></ShoppingCart>
                 <span>예약 관리</span>
               </div>
-              <CaretRight
-                size={12}
-                color={'#CBDAFF'}
-                weight={'bold'}
-              ></CaretRight>
+              <CaretRight size={12} color={'#CBDAFF'} weight={'bold'}></CaretRight>
             </div>
             <div className={'nav-item-box'}>
               <div className={'nav-item-name-box'}>
                 <User size={16} color={'#CBDAFF'} weight={'regular'}></User>
                 <span>회원 관리</span>
               </div>
-              <CaretRight
-                size={12}
-                color={'#CBDAFF'}
-                weight={'bold'}
-              ></CaretRight>
+              <CaretRight size={12} color={'#CBDAFF'} weight={'bold'}></CaretRight>
             </div>
           </div>
         </div>
--- app/(dashboard)/notice/page.tsx
+++ app/(dashboard)/notice/page.tsx
@@ -3,25 +3,11 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <>
-      <div className={'content-header'}>공지사항 목록..</div>
-      <div className={'content-body'}>
-        <div>공지사항 목록이 보여질 페이지입니다.</div>
-        <div
-          style={{
-            width: 500,
-            height: 300,
-            position: 'relative',
-          }}
-        >
-          <Image
-            src="https://images.unsplash.com/photo-1605936995788-2ccbd1d94da6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            priority
-            alt={'Marry christmas snoopy'}
-          />
-        </div>
+      <div className={'content-header'}>
+        <h3>공지사항 관리</h3>
+        <p>공지사항을 관리하는 페이지입니다. 공지사항을 등록, 수정, 삭제할 수 있습니다.</p>
       </div>
+      <div className={'content-body'}>테이블 영역..</div>
     </>
   );
 }
